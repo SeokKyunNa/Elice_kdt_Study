c132891be4f2ed445228273a0e036586ceb7c7ef
engsk <engsk1211@gmail.com>
2021-08-05 23:51:10 +0900

    백준 1문제 밖에 못 품..

---
+++ study_210805/baekjoon/s4_1920.py
@@ -0,0 +1,17 @@
+'''
+수 찾기
+'''
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+A = set(map(int, input().split()))
+
+M = int(input())
+X = list(map(int, input().split()))
+for i in range(M):
+    if X[i] in A:
+        print(1)
+    else:
+        print(0)
