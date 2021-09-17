ab860031d5eb2f41b896af04b039494d9f749240
SeokKyunNa <engsk1211@gmail.com>
2021-09-18 00:08:19 +0900

    greedy algorithm 동전0, 강의실 배정

--- withVSFe/210914_greedy_algorithm/s3_11399.py
+++ withVSFe/210914_greedy_algorithm/s3_11399.py
@@ -1,3 +1,17 @@
 '''
 ATM
 '''
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+P = list(map(int, input().split()))
+P.sort()
+result = 0
+accum = 0
+for num in P:
+    accum += num
+    result += accum
+
+print(result)
